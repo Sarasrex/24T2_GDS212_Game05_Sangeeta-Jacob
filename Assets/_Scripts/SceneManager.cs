5b02aca45241e92fef525322517881cfe0076a3d
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-03-15 00:40:36 +1100

    added basic main menu

---
+++ Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+
+    public GameObject playButton;
+
+
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void Play ()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+}
